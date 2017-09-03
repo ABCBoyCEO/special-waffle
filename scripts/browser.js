ebb3416dfecf20283f7680fc3973f03511ba5c6a
Un Known <jasonqan2004@gmail.com>
2017-09-03 07:49:36 -0400

    I'm dumb

--- scripts/browser.js
+++ scripts/browser.js
@@ -1,4 +1,4 @@
-if (window.navigator.userAgent.indexOf("MSIE ") > 0 || window.navigator.match(/Edge/i) || ! window.navigator.match(/Chrome|Safari|Firefox/i))
+if (window.navigator.userAgent.indexOf("MSIE ") > 0 || window.navigator.userAgent.match(/Edge/i) || ! window.navigator.userAgent.match(/Chrome|Safari|Firefox/i))
 {
     window.location = "https://www.mozilla.org/en-US/";
 }
