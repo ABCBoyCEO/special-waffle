3118466a3411b99143e9786534d08c8ae87fb54d
Logo <Logo121>
2017-09-03 20:53:00 +0800

    fixed a minor passive text bug that exists in the old versions. don't tell james pls

--- scripts/main.js
+++ scripts/main.js
@@ -276,7 +276,6 @@ $("div.passives").keyup(function() {
 
 function parseText(text, i) {
     i = SLEVEL[i];
-    text = text.replace(/<(br|div)>/g, "\n").replace(/<\/div>/g, "");
     text = text.replace(/\[\+\]/g, "+".repeat(i));
     text = text.replace(/(\-?\d+)\[(\+\d+|\-\d+)\]/g, function(a, b, c) { return Number(b) + Number(c) * i; });
     text = text.replace(/&gt;/g, ">").replace(/&lt;/g, "<");
@@ -284,8 +283,7 @@ function parseText(text, i) {
 }
 
 function cleanseText(text) { //To remove dumb Chrome rules on linebreaking and then some
-    text = text.replace(/<div><br><\/div>/g, "\n").replace(/<(br|div)>/g, "\n").replace(/<\/div>/g, "");
-    //rip readability, blame chrome. if this happens again, I'm gonna use the "Switch to Firefox" card.
+    text = text.replace(/<div><br><\/div>/g, "\n").replace("<div>","").replace(/<(br|div)>/g, "\n").replace(/<\/div>/g, "");
     return text;
 }
 
